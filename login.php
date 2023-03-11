d4d8eab71c1304e4e1f095ad8f2e13ea7739ca4a
agustinE13 <105616665+agustinE13@users.noreply.github.com>
2023-03-11 13:34:19 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -1,15 +1,31 @@
 <?php 
     include_once("header.php");
 ?>
-<form method="post" action="validar.php">
-  <label for="email">Correo electrónico:</label>
-  <input type="email" name="email" id="email" required>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <form method="post" action="validar.php">
+        <!-- input correo-->
+        <div class="form-group">
+          <label for="email" class="form-label mt-4">Correo electrónico:</label>
+          <input type="email" class="form-control" name="email" id="email" required>
+        </div>
+         <!-- input contraseña-->
+        <div class="form-group">
+          <label for="password" class="form-label mt-4">Contraseña:</label>
+          <input type="password" class="form-control" name="password" id="password" required>
+        </div>
+         <!-- input boton enviar-->
+        <div class="row mt-3 rounded">
+          <div class="col-md ">
+            <button type="submit" class="btn btn-outline-primary">Iniciar sesión</button>
+          </div>
+        </div>
 
-  <label for="password">Contraseña:</label>
-  <input type="password" name="password" id="password" required>
-
-  <button type="submit">Iniciar sesión</button>
-</form>
+      </form>
+    </div>
+  </div>
+</div>
 <?php     
     include_once("footer.php");
 ?>
